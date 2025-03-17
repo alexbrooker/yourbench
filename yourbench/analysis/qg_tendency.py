dc0f10cf046da6b4baa74aefea0e60503c1973a2
sumukshashidhar <sumuks2@illinois.edu>
2025-03-17 16:24:40 -0500

    add analysis of qg tendency

---
+++ yourbench/analysis/qg_tendency.py
@@ -0,0 +1,13 @@
+"""
+Analyze the tendency of a model to generate questions.
+"""
+
+from typing import List, Optional
+from loguru import logger
+
+def run(*args: str) -> None:
+    """
+    Run the analysis.
+    """
+    logger.info("Running question generation tendency analysis...")
+
